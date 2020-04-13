9c98b0b6fa927cd3cf8e004cb0ad283a06cbc245
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-04-13 17:30:03 -0500

    added photos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Nav from "./Nav.js";
+import travel from "../travel.png";
+import butterfly from "../butterfly.png";
+import fan from "../fan.png";
+import mb from "../mb-new.png";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import CardDeck from "react-bootstrap/CardDeck";
@@ -13,72 +17,89 @@ class Project extends React.Component {
       <div>
         <Nav />
         <Container fluid className="project">
-          <CardDeck>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="outline-light" size="lg" block>
-                  <Link to="/projects"> Checkout My Work</Link>
-                </Button>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="outline-light" size="lg" block>
-                  <Link to="/projects"> Checkout My Work</Link>
-                </Button>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
-          <CardDeck>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="outline-light" size="lg" block>
-                  <Link to="/projects"> Checkout My Work</Link>
-                </Button>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="outline-light" size="lg" block>
-                  <Link to="/projects"> Checkout My Work</Link>
-                </Button>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
+          <Container fluid="md">
+            <CardDeck>
+              <Card>
+                <Card.Img variant="top" className="fan" src={mb} />
+                <Card.Body>
+                  <Card.Title>Moments Beauty</Card.Title>
+                  <Card.Text>
+                    A cosmetic website complete with user authorization.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="outline-light" size="lg" block>
+                    <a href="https://momentsbeauty.herokuapp.com/">
+                      Moments Beauty
+                    </a>
+                  </Button>
+                </Card.Footer>
+              </Card>
+
+              <Card>
+                <Card.Img variant="top" className="fan" src={travel} />
+                <Card.Body>
+                  <Card.Title>Dunya Travel Guide</Card.Title>
+                  <Card.Text>
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This card has even longer
+                    content than the first to show that equal height action.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="outline-light" size="md">
+                    <a href="https://safa-express-mongo-example.herokuapp.com/travelguide">
+                      Live Site
+                    </a>
+                  </Button>
+
+                  <Button variant="outline-light" size="md">
+                    <a href="https://safa-express-mongo-example.herokuapp.com/travelguide">
+                      Code
+                    </a>
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </CardDeck>
+            <CardDeck>
+              <Card>
+                <Card.Img variant="top" className="fan" src={butterfly} />
+                <Card.Body>
+                  <Card.Title>Butterfly Effect</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to
+                    additional content.{" "}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="outline-light" size="lg">
+                    <a href="https://butterflyeffect-1.herokuapp.com/">
+                      Live Site
+                    </a>
+                  </Button>
+                  <Button variant="outline-light" size="lg">
+                    <a href="https://butterflyeffect-1.herokuapp.com/">Code</a>
+                  </Button>
+                </Card.Footer>
+              </Card>
+              <Card>
+                <Card.Img variant="top" className="fan" src={fan} />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to{" "}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="outline-light" size="lg" block>
+                    <a href="https://shirsi.github.io/">
+                      DC &amp; Mavel Fanpage
+                    </a>
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </CardDeck>
+          </Container>
         </Container>
       </div>
     );
