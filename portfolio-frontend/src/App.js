4d1e6f7d79293f7104e1ba8ca48041a786644520
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-04-13 10:57:50 -0500

    Deploy with only create-react-app-buildpack

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Projects from "./components/Projects.js";
+import Resume from "./components/Resume.js";
+import Home from "./components/Home.js";
+import Nav from "./components/Nav.js";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+/*
+********************************************************
+          Define
+********************************************************
+*/
+let baseURL = process.env.REACT_APP_BASEURL;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3003";
+} else {
+  baseURL = "https://butterflyeffect-1.herokuapp.com/";
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <div className="main">
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/projects"
+            render={(props) => <Projects {...props} baseURL={baseURL} />}
+          />
+          <Route
+            path="/resume"
+            render={(props) => <Resume {...props} baseURL={baseURL} />}
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
